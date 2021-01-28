20174401e21f77abee85d236a74126d8739c1ba5
turpp <white.tripp@icloud.com>
2021-01-28 02:21:24 -0600

    cosmeticly cleaned up code. removed old comments and fixed some line spacing and added new comments seperation big parts

--- js-files/index.js
+++ js-files/index.js
@@ -4,7 +4,7 @@ const holesDiv = document.getElementById('holes-div')
 const summaryDiv = document.getElementById('summary')
 
 
-
+//start
 roundSetup.addEventListener('submit', function(e){
     e.preventDefault()
     document.getElementById('title').innerText = 'myRound'
@@ -66,14 +66,9 @@ roundSetup.addEventListener('submit', function(e){
                         `
                         document.getElementById(`girr-hole-${i+1}`).onmousedown = GetCoordinates
                         document.getElementById(`firr-hole-${i+1}`).onmousedown = GetCoordinates
-
-
-
             }
-
             let coll = document.getElementsByClassName("collapsible");
             let i;
-            
             for (i = 0; i < coll.length; i++) {
               coll[i].addEventListener("click", function() {
                 this.classList.toggle("active");
@@ -89,74 +84,12 @@ roundSetup.addEventListener('submit', function(e){
 
             summaryDiv.innerHTML = `<button id='summary-btn' data-round=${holes[0].round_id} type='button'>Finalize Round</button>`
             document.getElementById('summary-btn').addEventListener('click', summary)
-            // let button;
-            // for(let i =0; i < holes.length; i++){
-            //     button = document.createElement('button')
-            //     button.type = 'submit'
-            //     button.className = 'hole'
-            //     mainDiv.appendChild(button)
-            // button.innerHTML += `<div data-hole=${holes[i].id} data-round=${holes[i].round_id} class='hole'>Hole ${i+1} score:__<div>`
-            // }
-
         })
     })
     
 })
-// document.addEventListener('click',function(e){
-//     console.log(e.target)
-//     let div;
-//     if(e.target.className == 'hole'){
-//         div = document.createElement('div')
-//         div.className = 'content'
-//         e.target.appendChild(div)
-//         fetch(`http://localhost:3000/rounds/${e.target.dataset.round}/holes/${e.target.dataset.hole}`).then(resp => resp.json()).then(function(hole){
-//             console.log(hole)
-//             div.innerHTML =`
-//                     <form id='hole-form'>
-//                     <span><img src='https://i.pinimg.com/564x/ec/17/22/ec1722fc44678bff1d60194f357e3769.jpg' alt='green' width='350' height='350'>
-//                     <img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASAAAACvCAMAAABqzPMLAAAAeFBMVEX///8AAAB4eHjy8vJWVlZ8e3w4ODju7u4NAAA8AAC7vL3p6em3uLn4+fmws7NYTE1xb28aAABkX1+qq6vNAAB8AAClAgRtAABrcHBWUVF1AABHAQRkAADT1dXh4uI7OzswAgQqAAAiAAC2AgSZAAAzAACZm5vGxscANfV+AAAB2klEQVR4nO3Uy04UQQCG0SlHZBgFkVEuXsAb+v5v6ErSTUi+jUm34Zxd7f58qarNBoDV279eesG6vDmdn892b49e8OD83cVh2me/e/9hMHF5dT0NdHzz8dPSk9bl88mX2RM7GuN2y1+nr8bYPA60/Ydf3H/vqUDHy0xZJ4GCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGg8FSg7TJT1unuUaDtxcnXwcS377fTQPvx42rpSety+fN6doXOd+PmJQ/G/f2vWaDfh/n5uTs7HJaeAM/DH3j5H6NdRWy2AAAAAElFTkSuQmCC' alt='fairway' width='350' height='350'>
-//                     </span>
-//                     <label>Putts</label>
-//                     <input type='number' name='putts' value = ${hole.putts ? hole.putts : 0}>
-//                     <label>Score</label>
-//                     <input type='number' name='score' value = ${hole.score ? hole.score : 0}>
-//                     <input type='submit' value='Submit Hole'>
-//                     </form>
-//                     <br>
-//             `
-//         }).then(function(){
-//             let holeFrom = document.getElementById('hole-form')
-//             holeFrom.addEventListener('submit', function(e){
-//                 e.preventDefault()
-//                 console.log('just sumbitted the edit form for hole')
-//             })
-//         })
 
-//     }
-// })
-
-// mainDiv.addEventListener('click', function(e){
-//     let holeForm;
-//     if (e.target.className == 'hole'){
-        
-//         e.target.innerHTML += `
-//         <form id='hole-form'>
-//         <span><img src='https://i.pinimg.com/564x/ec/17/22/ec1722fc44678bff1d60194f357e3769.jpg' alt='green' width='350' height'350'>
-//         <img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASAAAACvCAMAAABqzPMLAAAAeFBMVEX///8AAAB4eHjy8vJWVlZ8e3w4ODju7u4NAAA8AAC7vL3p6em3uLn4+fmws7NYTE1xb28aAABkX1+qq6vNAAB8AAClAgRtAABrcHBWUVF1AABHAQRkAADT1dXh4uI7OzswAgQqAAAiAAC2AgSZAAAzAACZm5vGxscANfV+AAAB2klEQVR4nO3Uy04UQQCG0SlHZBgFkVEuXsAb+v5v6ErSTUi+jUm34Zxd7f58qarNBoDV279eesG6vDmdn892b49e8OD83cVh2me/e/9hMHF5dT0NdHzz8dPSk9bl88mX2RM7GuN2y1+nr8bYPA60/Ydf3H/vqUDHy0xZJ4GCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGgIFAQKAgUBAoCBYGCQEGg8FSg7TJT1unuUaDtxcnXwcS377fTQPvx42rpSety+fN6doXOd+PmJQ/G/f2vWaDfh/n5uTs7HJaeAM/DH3j5H6NdRWy2AAAAAElFTkSuQmCC' alt='fairway' width='350' height='350'>
-//         </span>
-//         <label>Putts</label>
-//         <input type='number' name='putts'>
-//         <label>Score</label>
-//         <input type='number' name='score'>
-//         <input type='submit' value='Submit Hole'>
-//         </form>
-//         <br>
-//         `
-//     }
-    
-
-// })
-
+// submiting the form for the hole and sending info to the backend
 holesDiv.addEventListener('submit', function(e){
     e.preventDefault()
     let girId = window.event.path[0].childNodes[1].id
@@ -165,8 +98,6 @@ holesDiv.addEventListener('submit', function(e){
     let firDotArray = document.querySelectorAll(`#${firId} .dot`)
     let girCor = `${girDotArray[0].style.left.split('px')[0]}-${girDotArray[0].style.top.split('px')[0]}`
     let firCor =`${firDotArray[0].style.left.split('px')[0]}-${firDotArray[0].style.top.split('px')[0]}`
-    // I have the id i now need to query all and get the dots and then sytle left and top to get the cordinates to save
-    // console.log(girCor, firCor)
     fetch(`http://localhost:3000/holes/${e.target.dataset.hole}`,{
         method: 'PUT',
         headers: {
@@ -185,12 +116,8 @@ holesDiv.addEventListener('submit', function(e){
         let grandparent = parent.previousElementSibling
         grandparent.innerHTML = `Hole ${grandparent.dataset.holeNum} score: ${hole.score}`
 
-        // removeDot()
-        //going to hide dots
         let girDot = document.querySelectorAll(`#gir-hole-${grandparent.dataset.holeNum} .dot`)
         let firDot =document.querySelectorAll(`#fir-hole-${grandparent.dataset.holeNum} .dot`)
-        // girDot.forEach(dot => console.log(dot.style.top))
-        // firDot.forEach(dot => console.log(dot.style.left))
         let yGirDot = girDot[0].style.top
         let xGirDot = girDot[0].style.left
         let xFirDot = firDot[0].style.left
@@ -202,7 +129,6 @@ holesDiv.addEventListener('submit', function(e){
         // console.log(dotObj)
         
 
-        //
 
 
         e.target.innerHTML = `
@@ -223,7 +149,7 @@ holesDiv.addEventListener('submit', function(e){
 
     })
 })
-
+// actions for when you click on the summary button
 function summary(e){
     console.log('old=', e.target)
     fetch(`http://localhost:3000/rounds/${e.target.dataset.round}/summary`).then(resp => resp.json()).then(function(summary){
@@ -248,14 +174,6 @@ function summary(e){
 
     })
 
-    // summary.girArray.forEach(function(cordinate){
-    //     placeDot(cordinate, document.getElementById('gir-summar'))
-    // })
-    // summary.fwArray.forEach(function(cordinate){
-    //     placeDot(cordinate, document.getElementById('fir-summary'))
-    // })
-    // placeDot(summary.girArray, document.getElementById('gir-summary'))
-    // placeDot(summary.fwArray, document.getElementById('fir-summary'))
 
 }
 
