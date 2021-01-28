c42a3ff7dd654b9070493708c4d32572c9cecf03
turpp <white.tripp@icloud.com>
2021-01-28 15:39:22 -0600

    moved the creation of form and collabisble buttons to hole class

--- js-files/hole.js
+++ js-files/hole.js
@@ -7,4 +7,55 @@ class Hole{
         this.girloc = girloc
         this.putts = putts
     }
+
+    createButtonForms(holes){
+        let button;
+        let div;
+        for(let i = 0; i<holes.length; i++){
+            button = document.createElement('button')
+            button.type = 'button'
+            button.className = "collapsible"
+            button.dataset.holeNum = i+1
+            button.dataset.id = holes[i].id
+            holesDiv.appendChild(button)
+            button.innerHTML = `Hole ${i+1} score:__`
+            
+            div = document.createElement('div')
+            div.className = 'content'
+            holesDiv.appendChild(div)
+                div.innerHTML =`
+                    <form data-hole=${holes[i].id} id='hole-form'>
+                    <div id='gir-hole-${i+1}'> <img id='girr-hole-${i+1}' src='https://i.ibb.co/cgBBY05/GIR-image.jpg' alt='green' width='350' height='350'></div>
+                    <div id='fir-hole-${i+1}'> <img id='firr-hole-${i+1}' src='https://i.ibb.co/mv7cmHz/fir-image.jpg' alt='fairway' width='350' height='350'></div>
+                    <label>Par</label>
+                    <input type='number' name='par' value = ${holes[i].par ? holes[i].par : 0}>
+                    <label>Putts</label>
+                    <input type='number' name='putts' value = ${holes[i].putts ? holes[i].putts : 0}>
+                    <label>Score</label>
+                    <input type='number' name='score' value = ${holes[i].score ? holes[i].score : 0}>
+                    <input type='submit' value='Submit Hole'>
+                    </form>
+                    <br>
+                    `
+                    document.getElementById(`girr-hole-${i+1}`).onmousedown = GetCoordinates
+                    document.getElementById(`firr-hole-${i+1}`).onmousedown = GetCoordinates
+        }
+    }
+
+    makeButtonsCollapsible(){
+        let coll = document.getElementsByClassName("collapsible");
+            for (let i = 0; i < coll.length; i++) {
+              coll[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                let content = this.nextElementSibling;
+                if (content.style.display === "block") {
+                    // console.log(document.querySelectorAll(`#${this.nextElementSibling.childNodes[1].id} .dot`))
+                  content.style.display = "none";
+                } else {
+                  content.style.display = "block";
+                }
+              });
+            }
+    }
+
 }
--- js-files/index.js
+++ js-files/index.js
@@ -4,6 +4,7 @@ const holesDiv = document.getElementById('holes-div')
 const summaryDiv = document.getElementById('summary')
 const roundAdapter = new RoundAdapter('http://localhost:3000/rounds')
 const holeAdapter = new HoleAdapter()
+const holeObj = new Hole()
 //start
 roundSetup.addEventListener('submit', function(e){
     e.preventDefault()
@@ -34,51 +35,52 @@ roundSetup.addEventListener('submit', function(e){
         holeAdapter.fetchNewHoles(round)
         .then(resp => resp.json()).then(function(holes){
             mainDiv.innerHTML = ''
-            let button;
-            let div;
-            for(let i = 0; i<holes.length; i++){
-                button = document.createElement('button')
-                button.type = 'button'
-                button.className = "collapsible"
-                button.dataset.holeNum = i+1
-                button.dataset.id = holes[i].id
-                holesDiv.appendChild(button)
-                button.innerHTML = `Hole ${i+1} score:__`
+            holeObj.createButtonForms(holes)
+            // let button;
+            // let div;
+            // for(let i = 0; i<holes.length; i++){
+            //     button = document.createElement('button')
+            //     button.type = 'button'
+            //     button.className = "collapsible"
+            //     button.dataset.holeNum = i+1
+            //     button.dataset.id = holes[i].id
+            //     holesDiv.appendChild(button)
+            //     button.innerHTML = `Hole ${i+1} score:__`
                 
-                div = document.createElement('div')
-                div.className = 'content'
-                holesDiv.appendChild(div)
-                    div.innerHTML =`
-                        <form data-hole=${holes[i].id} id='hole-form'>
-                        <div id='gir-hole-${i+1}'> <img id='girr-hole-${i+1}' src='https://i.ibb.co/cgBBY05/GIR-image.jpg' alt='green' width='350' height='350'></div>
-                        <div id='fir-hole-${i+1}'> <img id='firr-hole-${i+1}' src='https://i.ibb.co/mv7cmHz/fir-image.jpg' alt='fairway' width='350' height='350'></div>
-                        <label>Par</label>
-                        <input type='number' name='par' value = ${holes[i].par ? holes[i].par : 0}>
-                        <label>Putts</label>
-                        <input type='number' name='putts' value = ${holes[i].putts ? holes[i].putts : 0}>
-                        <label>Score</label>
-                        <input type='number' name='score' value = ${holes[i].score ? holes[i].score : 0}>
-                        <input type='submit' value='Submit Hole'>
-                        </form>
-                        <br>
-                        `
-                        document.getElementById(`girr-hole-${i+1}`).onmousedown = GetCoordinates
-                        document.getElementById(`firr-hole-${i+1}`).onmousedown = GetCoordinates
-            }
-            let coll = document.getElementsByClassName("collapsible");
-            let i;
-            for (i = 0; i < coll.length; i++) {
-              coll[i].addEventListener("click", function() {
-                this.classList.toggle("active");
-                let content = this.nextElementSibling;
-                if (content.style.display === "block") {
-                    // console.log(document.querySelectorAll(`#${this.nextElementSibling.childNodes[1].id} .dot`))
-                  content.style.display = "none";
-                } else {
-                  content.style.display = "block";
-                }
-              });
-            }
+            //     div = document.createElement('div')
+            //     div.className = 'content'
+            //     holesDiv.appendChild(div)
+            //         div.innerHTML =`
+            //             <form data-hole=${holes[i].id} id='hole-form'>
+            //             <div id='gir-hole-${i+1}'> <img id='girr-hole-${i+1}' src='https://i.ibb.co/cgBBY05/GIR-image.jpg' alt='green' width='350' height='350'></div>
+            //             <div id='fir-hole-${i+1}'> <img id='firr-hole-${i+1}' src='https://i.ibb.co/mv7cmHz/fir-image.jpg' alt='fairway' width='350' height='350'></div>
+            //             <label>Par</label>
+            //             <input type='number' name='par' value = ${holes[i].par ? holes[i].par : 0}>
+            //             <label>Putts</label>
+            //             <input type='number' name='putts' value = ${holes[i].putts ? holes[i].putts : 0}>
+            //             <label>Score</label>
+            //             <input type='number' name='score' value = ${holes[i].score ? holes[i].score : 0}>
+            //             <input type='submit' value='Submit Hole'>
+            //             </form>
+            //             <br>
+            //             `
+            //             document.getElementById(`girr-hole-${i+1}`).onmousedown = GetCoordinates
+            //             document.getElementById(`firr-hole-${i+1}`).onmousedown = GetCoordinates
+            // }
+            // let coll = document.getElementsByClassName("collapsible");
+            // for (let i = 0; i < coll.length; i++) {
+            //   coll[i].addEventListener("click", function() {
+            //     this.classList.toggle("active");
+            //     let content = this.nextElementSibling;
+            //     if (content.style.display === "block") {
+            //         // console.log(document.querySelectorAll(`#${this.nextElementSibling.childNodes[1].id} .dot`))
+            //       content.style.display = "none";
+            //     } else {
+            //       content.style.display = "block";
+            //     }
+            //   });
+            // }
+            holeObj.makeButtonsCollapsible()
 
             summaryDiv.innerHTML = `<button id='summary-btn' data-round=${holes[0].round_id} type='button'>Finalize Round</button>`
             document.getElementById('summary-btn').addEventListener('click', summary)
