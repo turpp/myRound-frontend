b2e3858e935f72d01507a9d81f01385746cef0c7
turpp <white.tripp@icloud.com>
2021-01-29 17:33:42 -0600

    cleand up and formatted code to make it look pretty

--- js-files/dots.js
+++ js-files/dots.js
@@ -3,38 +3,32 @@ class Dots{
         this.xCor = x
         this.yCor = y
     }
-    FindPosition(oElement)
-    {
-      if(typeof( oElement.offsetParent ) != "undefined")
-      {
-        for(var posX = 0, posY = 0; oElement; oElement = oElement.offsetParent)
+
+    FindPosition(oElement){
+        if(typeof( oElement.offsetParent ) != "undefined")
         {
-          posX += oElement.offsetLeft;
-          posY += oElement.offsetTop;
-        }
-          return [ posX, posY ];
-        }
-        else
-        {
-          return [ oElement.x, oElement.y ];
-        }
+            for(let posX = 0, posY = 0; oElement; oElement = oElement.offsetParent){
+            posX += oElement.offsetLeft;
+            posY += oElement.offsetTop;
+            }
+            return [ posX, posY ];
+            }
+        else{
+            return [ oElement.x, oElement.y ];
+            }
     }
     
-    GetCoordinates(e)
-    
-    {
-      var PosX = 0;
-      var PosY = 0;
+    GetCoordinates(e){
+      let PosX = 0;
+      let PosY = 0;
     //   var ImgPos;
     //   ImgPos = FindPosition(gir);
       if (!e) var e = window.event;
-      if (e.pageX || e.pageY)
-      {
+      if (e.pageX || e.pageY){
         PosX = e.pageX;
         PosY = e.pageY;
       }
-      else if (e.clientX || e.clientY)
-        {
+      else if (e.clientX || e.clientY){
           PosX = e.clientX + document.body.scrollLeft
             + document.documentElement.scrollLeft;
           PosY = e.clientY + document.body.scrollTop
@@ -59,7 +53,6 @@ class Dots{
         div.style.left = array[0] + 'px';
         div.style.top = array[1] + 'px';
         location.appendChild(div)
-       
     }
     
     removeDot(){
@@ -69,8 +62,4 @@ class Dots{
     }
     // gir.onmousedown = GetCoordinates
     // fir.onmousedown = GetCoordinates
-
-
-
-
 }
--- js-files/hole.js
+++ js-files/hole.js
@@ -1,102 +1,102 @@
 class Hole{
-    constructor(id, round_id, score, fwloc, girloc, putts){
-        this.id = id
-        this.round_id = round_id
-        this.score = score
-        this.fwloc = fwloc
-        this.girloc = girloc
-        this.putts = putts
+  constructor(id, round_id, score, fwloc, girloc, putts){
+    this.id = id
+    this.round_id = round_id
+    this.score = score
+    this.fwloc = fwloc
+    this.girloc = girloc
+    this.putts = putts
+  }
+
+  createButtonForms(holes){
+    let button;
+    let div;
+    for(let i = 0; i<holes.length; i++){
+      button = document.createElement('button')
+      button.type = 'button'
+      button.className = "collapsible"
+      button.dataset.holeNum = i+1
+      button.dataset.id = holes[i].id
+      holesDiv.appendChild(button)
+      button.innerHTML = `Hole ${i+1} score:__`
+      
+      div = document.createElement('div')
+      div.className = 'content'
+      holesDiv.appendChild(div)
+      div.innerHTML =`
+        <form data-hole=${holes[i].id} id='hole-form'>
+        <div id='gir-hole-${i+1}'> <img id='girr-hole-${i+1}' src='https://i.ibb.co/cgBBY05/GIR-image.jpg' alt='green' width='350' height='350'></div>
+        <div id='fir-hole-${i+1}'> <img id='firr-hole-${i+1}' src='https://i.ibb.co/mv7cmHz/fir-image.jpg' alt='fairway' width='350' height='350'></div>
+        <label>Par</label>
+        <input type='number' name='par' value = ${holes[i].par ? holes[i].par : 0}>
+        <label>Putts</label>
+        <input type='number' name='putts' value = ${holes[i].putts ? holes[i].putts : 0}>
+        <label>Score</label>
+        <input type='number' name='score' value = ${holes[i].score ? holes[i].score : 0}>
+        <input type='submit' value='Submit Hole'>
+        </form>
+        <br>
+        `
+      document.getElementById(`girr-hole-${i+1}`).onmousedown = dotClass.GetCoordinates
+      document.getElementById(`firr-hole-${i+1}`).onmousedown = dotClass.GetCoordinates
     }
+  }
 
-    createButtonForms(holes){
-        let button;
-        let div;
-        for(let i = 0; i<holes.length; i++){
-            button = document.createElement('button')
-            button.type = 'button'
-            button.className = "collapsible"
-            button.dataset.holeNum = i+1
-            button.dataset.id = holes[i].id
-            holesDiv.appendChild(button)
-            button.innerHTML = `Hole ${i+1} score:__`
-            
-            div = document.createElement('div')
-            div.className = 'content'
-            holesDiv.appendChild(div)
-                div.innerHTML =`
-                    <form data-hole=${holes[i].id} id='hole-form'>
-                    <div id='gir-hole-${i+1}'> <img id='girr-hole-${i+1}' src='https://i.ibb.co/cgBBY05/GIR-image.jpg' alt='green' width='350' height='350'></div>
-                    <div id='fir-hole-${i+1}'> <img id='firr-hole-${i+1}' src='https://i.ibb.co/mv7cmHz/fir-image.jpg' alt='fairway' width='350' height='350'></div>
-                    <label>Par</label>
-                    <input type='number' name='par' value = ${holes[i].par ? holes[i].par : 0}>
-                    <label>Putts</label>
-                    <input type='number' name='putts' value = ${holes[i].putts ? holes[i].putts : 0}>
-                    <label>Score</label>
-                    <input type='number' name='score' value = ${holes[i].score ? holes[i].score : 0}>
-                    <input type='submit' value='Submit Hole'>
-                    </form>
-                    <br>
-                    `
-                    document.getElementById(`girr-hole-${i+1}`).onmousedown = dotClass.GetCoordinates
-                    document.getElementById(`firr-hole-${i+1}`).onmousedown = dotClass.GetCoordinates
-        }
+  makeButtonsCollapsible(){
+    let coll = document.getElementsByClassName("collapsible");
+    for (let i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          // console.log(document.querySelectorAll(`#${this.nextElementSibling.childNodes[1].id} .dot`))
+          content.style.display = "none";
+      } else {
+          content.style.display = "block";
+              }
+      });
     }
+  }
 
-    makeButtonsCollapsible(){
-        let coll = document.getElementsByClassName("collapsible");
-            for (let i = 0; i < coll.length; i++) {
-              coll[i].addEventListener("click", function() {
-                this.classList.toggle("active");
-                let content = this.nextElementSibling;
-                if (content.style.display === "block") {
-                    // console.log(document.querySelectorAll(`#${this.nextElementSibling.childNodes[1].id} .dot`))
-                  content.style.display = "none";
-                } else {
-                  content.style.display = "block";
-                }
-              });
-            }
+
+  getGirFirCor(){
+    let girId = window.event.path[0].childNodes[1].id
+    let firId = window.event.path[0].childNodes[3].id
+    let girDotArray = document.querySelectorAll(`#${girId} .dot`) 
+    let firDotArray = document.querySelectorAll(`#${firId} .dot`)
+    let girCor = `${girDotArray[0].style.left.split('px')[0]}-${girDotArray[0].style.top.split('px')[0]}`
+    let firCor =`${firDotArray[0].style.left.split('px')[0]}-${firDotArray[0].style.top.split('px')[0]}`
+    return [girCor, firCor]
+  }
+
+
+  collapseAndChangeCardTitle(e,hole){
+    let parent =e.target.parentNode
+    parent.style.display = 'none'
+    let grandparent = parent.previousElementSibling
+    grandparent.innerHTML = `Hole ${grandparent.dataset.holeNum} score: ${hole.score}`
+    return grandparent
+  }
+
+  getDotLocations(node){
+    let girDot = document.querySelectorAll(`#gir-hole-${node.dataset.holeNum} .dot`)
+    let firDot =document.querySelectorAll(`#fir-hole-${node.dataset.holeNum} .dot`)
+    let yGirDot = girDot[0].style.top
+    let xGirDot = girDot[0].style.left
+    let xFirDot = firDot[0].style.left
+    let yFirDot = firDot[0].style.top
+    let dotObj = {
+      girDot: [xGirDot.split('px')[0], yGirDot.split('px')[0]],
+      firDot: [xFirDot.split('px')[0], yFirDot.split('px')[0]]
     }
+    return dotObj
+  }
 
-
-    getGirFirCor(){
-      let girId = window.event.path[0].childNodes[1].id
-      let firId = window.event.path[0].childNodes[3].id
-      let girDotArray = document.querySelectorAll(`#${girId} .dot`) 
-      let firDotArray = document.querySelectorAll(`#${firId} .dot`)
-      let girCor = `${girDotArray[0].style.left.split('px')[0]}-${girDotArray[0].style.top.split('px')[0]}`
-      let firCor =`${firDotArray[0].style.left.split('px')[0]}-${firDotArray[0].style.top.split('px')[0]}`
-      return [girCor, firCor]
-    }
-
-
-    collapseAndChangeCardTitle(e,hole){
-      let parent =e.target.parentNode
-        parent.style.display = 'none'
-        let grandparent = parent.previousElementSibling
-        grandparent.innerHTML = `Hole ${grandparent.dataset.holeNum} score: ${hole.score}`
-        return grandparent
-    }
-
-    getDotLocations(node){
-      let girDot = document.querySelectorAll(`#gir-hole-${node.dataset.holeNum} .dot`)
-      let firDot =document.querySelectorAll(`#fir-hole-${node.dataset.holeNum} .dot`)
-      let yGirDot = girDot[0].style.top
-      let xGirDot = girDot[0].style.left
-      let xFirDot = firDot[0].style.left
-      let yFirDot = firDot[0].style.top
-      let dotObj = {
-          girDot: [xGirDot.split('px')[0], yGirDot.split('px')[0]],
-          firDot: [xFirDot.split('px')[0], yFirDot.split('px')[0]]
-      }
-      return dotObj
-    }
-
-    updateHoleForm(e,card,hole){
-      e.target.innerHTML = `
+  updateHoleForm(e,card,hole){
+    e.target.innerHTML = `
       <div id='gir-hole-${card.dataset.holeNum}'> <img id='gir-hole-${card.dataset.holeNum}' src='https://i.ibb.co/cgBBY05/GIR-image.jpg' alt='green' width='350' height='350'></div>
       <div id='fir-hole-${card.dataset.holeNum}'> <img id='fir-hole-${card.dataset.holeNum}' src='https://i.ibb.co/mv7cmHz/fir-image.jpg' alt='fairway' width='350' height='350'></div>
-<label>Par</label>
+      <label>Par</label>
       <input type='number' name='par' value = ${hole.par ? hole.par : 0}>
       <label>Putts</label>
       <input type='number' name='putts' value = ${hole.putts ? hole.putts : 0}>
@@ -104,6 +104,6 @@ class Hole{
       <input type='number' name='score' value = ${hole.score ? hole.score : 0}>
       <input type='submit' value='Submit Hole'>
       `
-    }
+  }
 
 }
