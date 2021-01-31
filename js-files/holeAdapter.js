4fec1d47348b5ba766d6a1d50d0b15e85d1fc6dc
turpp <white.tripp@icloud.com>
2021-01-31 01:39:22 -0600

    added an alert when form is not filled out correctly

--- js-files/holeAdapter.js
+++ js-files/holeAdapter.js
@@ -17,20 +17,52 @@ class HoleAdapter {
     }
 
     fetchEditHoles(e,girCor,firCor){
-       return fetch(`http://localhost:3000/holes/${e.target.dataset.hole}`,{
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                par: e.target.par.value,
-                score: e.target.score.value,
-                putts: e.target.putts.value,
-                girloc: girCor,
-                fwloc: firCor
-            })
-        })
 
+        switch(true){
+            case (e.target.score.value == 0):
+                alert("Please fill out score field.");
+                break;
+            case (e.target.par.value == 0):
+                alert("Please fill out par field.");
+                break;
+            case (e.target.putts.value == 0):
+                alert("Please fill out putts field.")
+                break;
+            default:
+                return fetch(`http://localhost:3000/holes/${e.target.dataset.hole}`,{
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        par: e.target.par.value,
+                        score: e.target.score.value,
+                        putts: e.target.putts.value,
+                        girloc: girCor,
+                        fwloc: firCor
+                    })
+                })
+
+
+        }
+
+    //     if(e.target.score.value == 0){
+    //         alert("Please Fill out score field");
+    //     }else {
+    //    return fetch(`http://localhost:3000/holes/${e.target.dataset.hole}`,{
+    //         method: 'PUT',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({
+    //             par: e.target.par.value,
+    //             score: e.target.score.value,
+    //             putts: e.target.putts.value,
+    //             girloc: girCor,
+    //             fwloc: firCor
+    //         })
+    //     })
+    //     }
     }
 
 }
